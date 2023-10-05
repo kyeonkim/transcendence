fd3a68d735433c49b2e67548406379d7a7f2decf
kyeonkim <kyeonkim@gmail.com>
2023-10-05 15:40:11 +0900

    Fix: 소스를 합칠 시 front 서버와 back 서버의 port 번호가 같으므로 back 서버 포트 4242 로 변경

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({origin: ["http://localhost:3000",  "http://10.13.8.3:3000"],});
-  await app.listen(3000);
+  await app.listen(4242);
 }
 bootstrap();
