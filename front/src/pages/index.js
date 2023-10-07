608c57f82440c41baf26e3c00bc9a101aa6f5bd4
CMS37 <rhtn037@naver.com>
2023-10-07 15:53:28 +0900

    init: Callback

---
+++ front/src/pages/index.js
@@ -0,0 +1,23 @@
+'use client'
+import React from 'react'
+
+
+const buttonStyle = {
+  color : 'white',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100vh',
+};
+
+export default function Home() {
+  const handleLogin = () => {
+    window.location.href = 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-a530d138cf1c33d448191cb250ee026f61f01d4d4cbbe62e0ff18ee285f9f290&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Foauth%2FCallback&response_type=code'
+  };
+
+  return (
+    <div style={buttonStyle}>
+      <button onClick={handleLogin}>login button</button>
+    </div>
+  )
+}
--- front/src/pages/oauth/Callback.tsx
+++ front/src/pages/oauth/Callback.tsx
@@ -1,49 +1,38 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 
-const Callback = (): JSX.Element => {
-  const router = useRouter();
-
+const Callback = () => {
   useEffect(() => {
-    const { code } = router.query;
-
-    if (code) {
-      const clientId = 'u-s4t2ud-a530d138cf1c33d448191cb250ee026f61f01d4d4cbbe62e0ff18ee285f9f290';
-      const clientSecret = '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';
-      const redirectUri = 'http://10.13.8.3:3000/oauth/callback';
-
-      console.log('code: ', code);
-
-      const url = 'https://api.intra.42.fr/oauth/token';
-      const data = {
-        grant_type: 'authorization_code',
-        client_id: clientId,
-        client_secret: clientSecret,
-        code: code,
-        redirect_uri: redirectUri,
-        scope: 'public'
-      };
-
-      const fetchData = async () => {
+    const handleCallback = async () => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const authorizationCode = urlParams.get('code');
+      
+      if (authorizationCode) {
         try {
-          const response = await axios.post(url, data, {
-            headers: {
-              'Content-Type': 'application/json',
-              'Access-Control-Allow-Origin': '*', // 임시적인 설정으로 CORS 정책 우회
-            },
+          const response = await axios.post('https://api.intra.42.fr/oauth/token', {
+            code: authorizationCode,
+            client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+            client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+            redirect_uri: 'http://localhost:3000/oauth/Callback',
+            grant_type: 'authorization_code'
           });
-          console.log('response: ', response.data);
+          console.log('Authorization Code:', authorizationCode);
+          console.log('data:', response.data);
+          //여기서 백앤드로 토큰 전송? -> 이후 백앤드에서 토큰으로 유저 정보 받아오기
         } catch (error) {
-          console.error('Error fetching data:', error);
+          console.error('Error:', error);
         }
-      };
+      }
+    };
 
-      fetchData();
-    }
-  }, [router.query]);
+    handleCallback();
+  }, []);
 
-  return <div>Callback Page</div>;
+  return (
+    <div>
+      <p>Callback Page</p>
+    </div>
+  );
 };
 
 export default Callback;
