526061774e869e8f1dd50014303c709c690e2b28
kyeonkim <kyeonkim@gmail.com>
2023-10-10 20:11:38 +0900

    Feat: Add module prisma> prisma 를 이용한 부분을 모듈화 시키기 위해

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -3,9 +3,10 @@ import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [HttpModule, AuthModule],
+  imports: [HttpModule, AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
