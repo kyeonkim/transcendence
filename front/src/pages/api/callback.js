5710801d63da83ba5240366e602eeb77303bc3c8
Min-soo Cho <min-cho@c8r4s14.42seoul.kr>
2023-10-10 21:09:40 +0900

    feat:

--- front/src/pages/api/callback.js
+++ front/src/pages/api/callback.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   const { code } = req.query;
-
-  console.log('req: ', req.query)
   if (code)
   {
     try {
@@ -11,10 +9,9 @@ export default async function handler(req, res) {
         code: code,
         client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
         client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
-        redirect_uri: 'http://10.28.4.14:3000/oauth/Login',
+        redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_URI,
         grant_type: 'authorization_code'
       });
-  
       res.status(200).json(response.data);
     } catch (error) {
       res.status(500).json({ error: 'Internal server error 2' });
--- front/src/pages/oauth/Login.tsx
+++ front/src/pages/oauth/Login.tsx
@@ -4,9 +4,9 @@ import { useRouter } from 'next/router';
 
 const Login = () => {
   const router = useRouter();
+  const { code } = router.query;
 
   useEffect(() => {
-    const { code } = router.query;
 
     console.log('code:', code);
     if (code) {
@@ -17,23 +17,22 @@ const Login = () => {
               code: code
             }
           });
-          console.log('data:', response.data);
           const userData = await axios.post('http://10.28.4.11:4242/auth/token' , response.data);
           if (userData)
             router.push('/main');
           else
             router.push('/signup');
         } catch (error) {
-          console.error('Error:', error);
+          console.error('Error:', error.response.data);
           }
         }
         handleCallback();
       };
-    }, [router.query]);
+    }, [code]);
 
   return (
     <div>
-      <p>Callback Page</p>
+      <p>Callback Page - Loading</p>
     </div>
   );
 };
