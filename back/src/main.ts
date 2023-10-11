29d599c687142c939cbb932c1d5dc7d0cd1fe9b7
kyeonkim <kyeonkim@gmail.com>
2023-10-11 18:42:03 +0900

    Feat: RESTful API 공유를 위한 스웨거 설치도입

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({origin: ["http://localhost:3000",  "http://10.13.8.3:3000"],});
+  
+  const config = new DocumentBuilder()
+  .setTitle('Transcendence')
+  .setDescription('The Transcendence API description')
+  .setVersion('0.1')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(4242);
 }
 bootstrap();
