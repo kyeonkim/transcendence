65574b2fb4f8930a24dcc009f67f92afa8a65a27
Junhyung Park <junhyupa@c3r9s2.42seoul.kr>
2023-10-17 19:42:19 +0900

    Remove: 필요없는 파일들 삭제 -app.service app.contorller 와 테스트 파일들 삭제

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [HttpModule, PrismaModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HttpModule, PrismaModule, UserModule, AuthModule]
 })
 export class AppModule {}
