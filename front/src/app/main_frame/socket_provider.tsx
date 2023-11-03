87a5009546a4fb52d23c8f7a4584f10c55ee7a9a
kshim1208 <a41533503@gmail.com>
2023-11-03 18:20:13 +0900

    Fix: trying to fix that socket keeps opened twice
    with React.memo and define socket variable outside of component, seems
    like socket opens just once when MainFrame renders.
    
    still there are many unsolved problem.

--- front/src/app/main_frame/socket_provider.tsx
+++ front/src/app/main_frame/socket_provider.tsx
@@ -1,9 +1,24 @@
+
 "use client"
+
 import React, { memo, createContext, useContext } from 'react';
 import { Socket } from "socket.io-client"
 
 import { useCookies } from "next-client-cookies"
 import { io } from "socket.io-client";
+
+import { getCookie } from 'cookies-next';
+
+
+const socket = io(`${process.env.NEXT_PUBLIC_WEBSOCKET_URL}`,
+{
+  query: {
+      user_id: getCookie('user_id'),
+  },
+  extraHeaders: {
+      token: getCookie('access_token'),
+  },
+});
 
 const ChatSocketContext = createContext<Socket>(null);
 
@@ -14,19 +29,6 @@ export const useChatSocket = () => {
 
 export function ChatSocket ({ children }: any) {
 
-  // const socket = io(`${process.env.NEXT_PUBLIC_WEBSOCKET_URL}`);
-
-  const cookies = useCookies();
-
-  const socket = io(`${process.env.NEXT_PUBLIC_WEBSOCKET_URL}`,
-  {
-    query: {
-        user_id: cookies.get('user_id'),
-    },
-    extraHeaders: {
-        token: cookies.get('access_token'),
-    },
-  });
 
   socket.on("connect" , () => {
     console.log('socket connected - ', socket.id); // value
@@ -46,3 +48,55 @@ export function ChatSocket ({ children }: any) {
 export const memoChatSocket = memo(ChatSocket);
 
 export default ChatSocket;
+
+
+
+// "use client"
+
+// import React, { memo, createContext, useContext } from 'react';
+// import { Socket } from "socket.io-client"
+
+// import { useCookies } from "next-client-cookies"
+// import { io } from "socket.io-client";
+
+// const ChatSocketContext = createContext<Socket>(null);
+
+
+// export const useChatSocket = () => {
+//   return useContext(ChatSocketContext);
+// };
+
+// export function ChatSocket ({ children }: any) {
+
+//   // const socket = io(`${process.env.NEXT_PUBLIC_WEBSOCKET_URL}`);
+
+//   const cookies = useCookies();
+
+//   const socket = io(`${process.env.NEXT_PUBLIC_WEBSOCKET_URL}`,
+//   {
+//     query: {
+//         user_id: cookies.get('user_id'),
+//     },
+//     extraHeaders: {
+//         token: cookies.get('access_token'),
+//     },
+//   });
+
+//   socket.on("connect" , () => {
+//     console.log('socket connected - ', socket.id); // value
+//   })
+
+//   socket.on("disconnect", () => {
+//     console.log('socket diconnected - ', socket.id); // undefined
+//   });
+
+//   return (
+//     <ChatSocketContext.Provider value={socket}>
+//       {children}
+//     </ChatSocketContext.Provider>
+//   );
+// }
+
+// export const memoChatSocket = memo(ChatSocket);
+
+// export default ChatSocket;
