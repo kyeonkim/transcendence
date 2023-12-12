346e47d362831c620656554691e4ef9c7afb47eb
Min-soo Cho <min-cho@c3r8s3.42seoul.kr>
2023-12-12 17:55:35 +0900

    feat: 채팅방 옵션기능 추가, 차단랜더링조건 추가

--- front/src/app/entrance/page.tsx
+++ front/src/app/entrance/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@mui/material/Button';
 import type { Engine } from "tsparticles-engine";
 import { ISourceOptions } from "tsparticles-engine";
@@ -7,17 +7,12 @@ import { useCallback } from 'react';
 import particlesOptions from "../particles.json";
 import { loadFull } from "tsparticles";
 import Particles from "react-tsparticles";
-
-const buttonStyle = {
-  color : 'white',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '95vh',
-};
-
+import { Box, TextField } from '@mui/material';
 
 export default function Home() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
   const particlesInit = useCallback(async (engine: Engine) => {
     await loadFull(engine);
   }, []);
@@ -26,19 +21,39 @@ export default function Home() {
     window.location.href = process.env.NEXT_PUBLIC_REDIRECT_URL;
   };
 
-
-function ButtonUsage() {
-  return <Button variant="contained" onClick={handleLogin}>Sign with 42</Button>;
-
-}
-
+  const handleSignUp = () => {
+  };
 
   return (
-    <>
-    <Particles options={particlesOptions as ISourceOptions} init={particlesInit} />
-    <div style={buttonStyle}>
-      <ButtonUsage />
-    </div>
-    </>
-  ) 
+    <Box sx={{ position: 'relative', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Particles options={particlesOptions as ISourceOptions} init={particlesInit} />
+      <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
+        <Box sx={{ marginBottom: '16px' }}>
+          <TextField
+            label="ID"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            variant="outlined"
+            sx={{ marginRight: '8px' }}
+          />
+          <TextField
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            variant="outlined"
+            sx={{ marginRight: '8px' }}
+          />
+        </Box>
+        <Box sx={{ marginBottom: '16px' }}>
+          <Button variant="contained" onClick={handleLogin} sx={{ marginRight: '8px' }}>
+            Sign In
+          </Button>
+          <Button variant="contained" onClick={handleSignUp}>
+            Sign Up
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
